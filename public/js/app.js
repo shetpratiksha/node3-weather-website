3f47fe672c9dd5d4481aa9613bff985e65c34364
Pratiksha <pratiksha.shet@zapcg.com>
2020-04-04 17:39:19 +0530

    Removing unnecessary console.log from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-console.log('Js serving the page')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) =>{
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
-
 const weatherForm = document.querySelector('form');
 const inputSearch = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
